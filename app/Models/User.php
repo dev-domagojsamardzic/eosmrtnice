8380dbcecf70a4fcbf8db9cdd13312cb95fa2819
Domagoj Samardžić <dev.domagojsamardzic@gmail.com>
2024-02-24 17:27:24 +0100

    Update PHPDoc on User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,10 +2,23 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+
+/**
+ * @property    int         id
+ * @property    string      name
+ * @property    string      email
+ * @property    string      type
+ * @property    Carbon      email_verified_at
+ * @property    string      password
+ * @property    string      remember_token
+ * @property    Carbon      created_at
+ * @property    Carbon      updated_at
+ */
 
 class User extends Authenticatable implements MustVerifyEmail
 {
