77d66012c00d2d9df8364d3314393883632befa6
Domagoj Samardžić <dev.domagojsamardzic@gmail.com>
2024-02-24 16:51:58 +0100

    Create default admin account

---
+++ database/seeders/AdminUserSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class AdminUserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('users')->insert([
+            'name' => 'Leon Rešetar',
+            'email' => 'test@email.com',
+            'type' => 'admin',
+            'email_verified_at' => now(),
+            'password' => Hash::make('leonresetar2024'),
+            'remember_token' => null,
+            'created_at' => now(),
+            'updated_at' => null,
+        ]);
+    }
+}
