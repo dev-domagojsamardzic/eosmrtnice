185e8bd579f0f884c84ad89aef92da994b912e93
dev-domagojsamardzic <dev.domagojsamardzic@gmail.com>
2024-02-29 17:12:15 +0100

    Change text on form

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -16,7 +16,7 @@
                             <div class="col-lg-6">
                                 <div class="p-5">
                                     <div class="text-center">
-                                        <h1 class="h4 text-gray-900 mb-4">{{ __('auth.welcome') }}</h1>
+                                        <h1 class="h4 text-gray-900 mb-4">{{ config('app.name') }}</h1>
                                     </div>
                                     <form>
                                         <div class="form-group">
@@ -37,27 +37,21 @@
                                         <div class="form-group">
                                             <div class="custom-control custom-checkbox small">
                                                 <input type="checkbox" class="custom-control-input" id="customCheck">
-                                                <label class="custom-control-label" for="customCheck">Remember
-                                                    Me</label>
+                                                <label class="custom-control-label" for="customCheck">
+                                                    {{ __('auth.labels.remember_me') }}
+                                                </label>
                                             </div>
                                         </div>
-                                        <a href="index.html" class="btn btn-primary btn-user btn-block">
-                                            Login
-                                        </a>
-                                        <hr>
-                                        <a href="index.html" class="btn btn-google btn-user btn-block">
-                                            <i class="fab fa-google fa-fw"></i> Login with Google
-                                        </a>
-                                        <a href="index.html" class="btn btn-facebook btn-user btn-block">
-                                            <i class="fab fa-facebook-f fa-fw"></i> Login with Facebook
+                                        <a href="#" class="btn btn-primary btn-user btn-block">
+                                            {{ __('auth.log_in') }}
                                         </a>
                                     </form>
                                     <hr>
                                     <div class="text-center">
-                                        <a class="small" href="forgot-password.html">Forgot Password?</a>
+                                        <a class="small" href="#">{{ __('auth.labels.forgot_password') }}</a>
                                     </div>
                                     <div class="text-center">
-                                        <a class="small" href="register.html">Create an Account!</a>
+                                        <a class="small" href="#">{{ __('auth.create_account') }}</a>
                                     </div>
                                 </div>
                             </div>
