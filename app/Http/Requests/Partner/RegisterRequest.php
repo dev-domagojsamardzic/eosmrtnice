537c2bc36d607f3dc0fa668b0a37400248ad23d6
dev-domagojsamardzic <dev.domagojsamardzic@gmail.com>
2024-03-06 17:08:42 +0100

    Create new request with validation

---
+++ app/Http/Requests/Partner/RegisterRequest.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Http\Requests\Partner;
+
+use App\Models\User;
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rules\Password;
+
+/**
+ * @property    string      first_name
+ * @property    string      last_name
+ * @property    string      email
+ * @property    string      sex
+ * @property    string      password
+ * @property    string      confirm_password
+ * -----------------------------------------
+ * @property    string      company_title
+ * @property    string      company_address
+ * @property    string      company_town
+ * @property    string      company_zipcode
+ * @property    string      company_oib
+ * @property    string      company_email
+ * @property    string      company_phone
+ * @property    string      company_mobile_phone
+ */
+
+class RegisterRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return !auth()->check();
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'first_name' => ['required', 'string', 'max:255'],
+            'last_name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:'.User::class],
+            'sex' => ['required', 'in:m,f'],
+            'password' => ['required', 'confirmed', Password::defaults()],
+
+            'company_title' => ['required', 'string', 'max:255'],
+            'company_address' => ['string', 'max:512'],
+            'company_town' => ['string', 'max:255'],
+            'company_zipcode' => ['number', 'max:6'],
+            'company_oib' => ['number', 'digits:11'],
+            'company_email' => ['string', 'email', 'max:255'],
+            'company_phone' => ['string', 'max:64'],
+            'company_mobile_phone' => ['string', 'max:64']
+        ];
+    }
+}
