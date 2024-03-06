a40b82ee21ce8c12dd445a21b68d9109b62258e3
dev-domagojsamardzic <dev.domagojsamardzic@gmail.com>
2024-03-06 18:37:00 +0100

    Register profile routes

--- routes/admin.php
+++ routes/admin.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProfileController;
 
-Route::get('/dashboard', function () {
+Route::get('/dashboard', static function () {
     return view('admin/dashboard');
 })->name('dashboard');
+
+Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
