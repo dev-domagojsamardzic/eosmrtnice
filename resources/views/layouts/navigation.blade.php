ffda9e16c8a1a398e9b064246a402fea68945ab4
dev-domagojsamardzic <dev.domagojsamardzic@gmail.com>
2024-03-06 18:36:06 +0100

    Implement helpers function

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -164,7 +164,7 @@
                 <!-- Dropdown - User Information -->
                 <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in"
                      aria-labelledby="userDropdown">
-                    <a class="dropdown-item" href="#">
+                    <a class="dropdown-item" href="{{ route(auth_user_type() . '.profile.edit') }}">
                         <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
                         Profile
                     </a>
