2d5a6b2a95a5f9e6838a5624569b0e14e194a6e3
dev-domagojsamardzic <dev.domagojsamardzic@gmail.com>
2024-03-07 18:17:55 +0100

    Register PartnerController routes

--- routes/admin.php
+++ routes/admin.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\Admin\PartnerController;
 
 Route::get('/dashboard', static function () {
     return view('admin/dashboard');
@@ -9,3 +10,5 @@ Route::get('/dashboard', static function () {
 
 Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
 Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+
+Route::resource('partners',PartnerController::class);
