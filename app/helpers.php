2e8d2901b04f9cb7ac92898128de850d7347de58
dev-domagojsamardzic <dev.domagojsamardzic@gmail.com>
2024-03-11 20:22:13 +0100

    Create boolean function for each role (user type)

--- app/helpers.php
+++ app/helpers.php
@@ -1,6 +1,8 @@
 <?php
 
-if(!function_exists('auth_user_type')) {
+use App\Enums\UserType;
+
+if (!function_exists('auth_user_type')) {
     /**
      * Return type of currently authenticated user
      * @return string
@@ -8,5 +10,39 @@ if(!function_exists('auth_user_type')) {
     function auth_user_type(): string
     {
         return auth()?->user()?->type?->value ?? '';
+    }
+}
+
+if (!function_exists('is_admin')) {
+
+    /**
+     * Check if currently authenticated user is admin
+     * @return bool
+     */
+    function is_admin(): bool
+    {
+        return auth()?->user()?->type === UserType::ADMIN;
+    }
+}
+
+if (!function_exists('is_partner')) {
+    /**
+     * Check if currently authenticated user is admin
+     * @return bool
+     */
+    function is_partner(): bool
+    {
+        return auth()?->user()?->type === UserType::PARTNER;
+    }
+}
+
+if (!function_exists('is_user')) {
+    /**
+     * Check if currently authenticated user is admin
+     * @return bool
+     */
+    function is_user(): bool
+    {
+        return auth()?->user()?->type === UserType::USER;
     }
 }
