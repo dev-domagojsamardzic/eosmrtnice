28287b1a8660f2ebc8897bf1b96a48d217f0eeff
dev-domagojsamardzic <dev.domagojsamardzic@gmail.com>
2024-03-11 21:42:01 +0100

    Delete show route

--- routes/admin.php
+++ routes/admin.php
@@ -12,4 +12,4 @@ Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit')
 Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
 
 Route::resource('partners',PartnerController::class)
-    ->only(['index', 'show', 'edit', 'update']);
+    ->only(['index', 'edit', 'update']);
