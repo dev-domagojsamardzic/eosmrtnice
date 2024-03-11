d6be9459779f2e5cd321143c7df8e95fa0097c39
dev-domagojsamardzic <dev.domagojsamardzic@gmail.com>
2024-03-11 19:58:30 +0100

    create new functions

--- app/Http/Controllers/Admin/PartnerController.php
+++ app/Http/Controllers/Admin/PartnerController.php
@@ -2,7 +2,11 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\Enums\UserType;
 use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
 use Illuminate\View\View;
 
 class PartnerController extends Controller
@@ -12,6 +16,47 @@ class PartnerController extends Controller
      */
     public function index(): View
     {
-        return view('partner.index');
+        return view('admin.partners.index');
+    }
+
+    /**
+     * @param User $partner
+     * @return View
+     */
+    public function show(User $partner): View
+    {
+        return view('admin.partners.form', ['partner' => $partner]);
+    }
+
+    /**
+     * Show partner edit form
+     * @param User $partner
+     * @return View
+     */
+    public function edit(User $partner): View
+    {
+        return $this->form($partner, 'edit');
+    }
+
+    public function update(User $partner, Request $request): RedirectResponse
+    {
+
+    }
+
+    /**
+     * Display resource's form
+     * @param User $partner
+     * @param string $action
+     * @return View
+     */
+    private function form(User $partner, string $action): View
+    {
+        $route = match($action) {
+            'edit' => route(auth_user_type() . '.partners.edit', ['partner' => $partner]),
+            'create' => route(auth_user_type() . '.partners.create'),
+            default => ''
+        };
+
+        return view('admin.partners.form', ['partner' => $partner, 'action' => $action, 'route' => $route]);
     }
 }
