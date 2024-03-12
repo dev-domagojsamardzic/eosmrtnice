568381f5ee31a1c2cf9d98d3b41983ea7ad0d64a
dev-domagojsamardzic <dev.domagojsamardzic@gmail.com>
2024-03-12 17:05:20 +0100

    Increase margin

--- resources/views/admin/partners/index.blade.php
+++ resources/views/admin/partners/index.blade.php
@@ -1,6 +1,6 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-weight-bolder mb-4">
+        <h2 class="font-weight-bolder mb-5">
             {{ __('admin.partners') }}
         </h2>
     </x-slot>
