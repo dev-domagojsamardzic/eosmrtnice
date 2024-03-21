4803c17f42bc3aba60ac3ec470eeb554fed03e4c
dev-domagojsamardzic <dev.domagojsamardzic@gmail.com>
2024-03-21 18:16:45 +0100

    Update seeder, add more of everything

--- database/seeders/CompanySeeder.php
+++ database/seeders/CompanySeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Enums\CompanyType;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
@@ -14,7 +15,93 @@ class CompanySeeder extends Seeder
     {
         DB::table('companies')->insert([
             'user_id' => 2,
+            'type' => CompanyType::FUNERAL,
+            'title' => 'Pogrebno poduzeće Smrtić',
+            'address' => 'Nikole Tesle 14',
+            'town' => 'Zagreb',
+            'zipcode' => '10000',
+            'county_id' => 21,
+            'oib' => '11899764352',
+            'email' => 'kompanija@email.com',
+            'phone' => '000339812',
+            'mobile_phone' => '003876788943',
+            'active' => 1
+        ]);
+        DB::table('companies')->insert([
+            'user_id' => 2,
+            'type' => CompanyType::MASONRY,
+            'title' => 'Kameno poduzeće Masonić',
+            'address' => 'Nikole Tesle 14',
+            'town' => 'Slavonski Brod',
+            'zipcode' => '35000',
+            'county_id' => 12,
+            'oib' => '11899764352',
+            'email' => 'kompanija@email.com',
+            'phone' => '000339812',
+            'mobile_phone' => '003876788943',
+            'active' => 1
+        ]);
+        DB::table('companies')->insert([
+            'user_id' => 2,
+            'type' => CompanyType::FLOWERS,
+            'title' => 'Cvjećarnica Maslačak',
+            'address' => 'Nikole Tesle 14',
+            'town' => 'Zagreb',
+            'zipcode' => '10000',
+            'county_id' => 21,
+            'oib' => '11899764352',
+            'email' => 'kompanija@email.com',
+            'phone' => '000339812',
+            'mobile_phone' => '003876788943',
+            'active' => 0
+        ]);
+        DB::table('companies')->insert([
+            'user_id' => 2,
+            'type' => CompanyType::FLOWERS,
             'title' => 'Pogrebno poduzeće',
+            'address' => 'Nikole Tesle 14',
+            'town' => 'Karlovac',
+            'zipcode' => '21300',
+            'county_id' => 4,
+            'oib' => '11899764352',
+            'email' => 'kompanija@email.com',
+            'phone' => '000339812',
+            'mobile_phone' => '003876788943',
+            'active' => 1
+        ]);
+        DB::table('companies')->insert([
+            'user_id' => 2,
+            'type' => CompanyType::MASONRY,
+            'title' => 'Kamenjarka',
+            'address' => 'Nikole Tesle 14',
+            'town' => 'Zagreb',
+            'zipcode' => '10000',
+            'county_id' => 21,
+            'oib' => '11899764352',
+            'email' => 'kompanija@email.com',
+            'phone' => '000339812',
+            'mobile_phone' => '003876788943',
+            'active' => 0
+        ]);
+
+        DB::table('companies')->insert([
+            'user_id' => 3,
+            'type' => CompanyType::FLOWERS,
+            'title' => 'Cvjećarska radnja Bartina',
+            'address' => 'Nikole Tesle 14',
+            'town' => 'Zagreb',
+            'zipcode' => '10000',
+            'county_id' => 21,
+            'oib' => '11899764352',
+            'email' => 'kompanija@email.com',
+            'phone' => '000339812',
+            'mobile_phone' => '003876788943',
+            'active' => 1
+        ]);
+        DB::table('companies')->insert([
+            'user_id' => 3,
+            'type' => CompanyType::FUNERAL,
+            'title' => 'Pogrebno poduzeće Piramida',
             'address' => 'Nikole Tesle 14',
             'town' => 'Zagreb',
             'zipcode' => '10000',
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,7 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Enums\UserSex;
 use App\Enums\UserType;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
@@ -16,37 +18,124 @@ class UserSeeder extends Seeder
     {
         // Admin
         DB::table('users')->insert([
+            'id' => 1,
             'first_name' => 'Admin',
             'last_name' => 'Administratorović',
             'email' => 'admin@email.com',
             'type' => UserType::ADMIN,
-            'sex' => 'M',
+            'sex' => UserSex::MALE,
             'email_verified_at' => now(),
             'password' => Hash::make('admin'),
             'remember_token' => null,
             'created_at' => now(),
             'updated_at' => null,
         ]);
-        // Partner
+
+        // Partners
         DB::table('users')->insert([
+            'id' => 2,
             'first_name' => 'Partner',
-            'last_name' => 'Partnerović',
+            'last_name' => 'Partner',
             'email' => 'partner@email.com',
             'type' => UserType::PARTNER,
-            'sex' => 'M',
+            'sex' => UserSex::MALE,
             'email_verified_at' => now(),
             'password' => Hash::make('partner'),
             'remember_token' => null,
             'created_at' => now(),
             'updated_at' => null,
         ]);
-        // User
+
         DB::table('users')->insert([
-            'first_name' => 'Korisnica',
-            'last_name' => 'Korisniković',
+            'id' => 3,
+            'first_name' => 'Matej',
+            'last_name' => 'Partner2',
+            'email' => 'partner2@email.com',
+            'type' => UserType::PARTNER,
+            'sex' => UserSex::FEMALE,
+            'email_verified_at' => now(),
+            'password' => Hash::make('partner'),
+            'remember_token' => null,
+            'created_at' => now(),
+            'updated_at' => null,
+        ]);
+
+        DB::table('users')->insert([
+            'id' => 4,
+            'first_name' => 'Ivan',
+            'last_name' => 'Partner3',
+            'email' => 'partnerovski@email.com',
+            'type' => UserType::PARTNER,
+            'sex' => UserSex::FEMALE,
+            'active' => 0,
+            'email_verified_at' => now(),
+            'password' => Hash::make('partner'),
+            'remember_token' => null,
+            'created_at' => now(),
+            'updated_at' => null,
+        ]);
+
+        // Users
+        DB::table('users')->insert([
+            'id' => 5,
+            'first_name' => 'Domagoj',
+            'last_name' => 'Samardžić',
             'email' => 'dokisb1001@gmail.com',
             'type' => UserType::MEMBER,
             'sex' => 'F',
+            'email_verified_at' => now(),
+            'password' => Hash::make('user'),
+            'remember_token' => null,
+            'created_at' => now(),
+            'updated_at' => null,
+        ]);
+        DB::table('users')->insert([
+            'id' => 6,
+            'first_name' => 'Marko',
+            'last_name' => 'Martić',
+            'email' => 'user@email.com',
+            'type' => UserType::MEMBER,
+            'sex' => UserSex::MALE,
+            'email_verified_at' => now(),
+            'password' => Hash::make('user'),
+            'remember_token' => null,
+            'created_at' => now(),
+            'updated_at' => null,
+        ]);
+        DB::table('users')->insert([
+            'id' => 7,
+            'first_name' => 'Antonela',
+            'last_name' => 'Skorupski',
+            'email' => 'user2@email.com',
+            'type' => UserType::MEMBER,
+            'sex' => 'F',
+            'email_verified_at' => now(),
+            'password' => Hash::make('user'),
+            'remember_token' => null,
+            'created_at' => now(),
+            'updated_at' => null,
+        ]);
+        DB::table('users')->insert([
+            'id' => 8,
+            'first_name' => 'Korisnik',
+            'last_name' => 'Korisniković',
+            'email' => 'user3@email.com',
+            'type' => UserType::MEMBER,
+            'sex' => UserSex::MALE,
+            'active' => 0,
+            'email_verified_at' => now(),
+            'password' => Hash::make('user'),
+            'remember_token' => null,
+            'created_at' => now(),
+            'updated_at' => null,
+        ]);
+        DB::table('users')->insert([
+            'id' => 9,
+            'first_name' => 'Andrija',
+            'last_name' => 'Sivrić',
+            'email' => 'user4@email.com',
+            'type' => UserType::MEMBER,
+            'sex' => UserSex::MALE,
             'email_verified_at' => now(),
             'password' => Hash::make('user'),
             'remember_token' => null,
