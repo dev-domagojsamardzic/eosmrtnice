67bfe326157dd156801d09e5a94d3b01ff74d4fb
dev-domagojsamardzic <dev.domagojsamardzic@gmail.com>
2024-04-11 20:38:38 +0200

    Update County model

--- app/Models/County.php
+++ app/Models/County.php
@@ -4,8 +4,26 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+use Illuminate\Support\Collection;
 
+/**
+ * @property-read   int                 id
+ * @property        string              title
+ * ------------------------------------------
+ * @property        Collection|City     cities
+ */
 class County extends Model
 {
     use HasFactory;
+
+    /**
+     * All cities that belong to a county
+     * @return HasMany
+     */
+    public function cities(): HasMany
+    {
+        return $this->hasMany(City::class);
+    }
 }
