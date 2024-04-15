b93eb3990985e5a481349702f0f81b0b8117ee1f
dev-domagojsamardzic <dev.domagojsamardzic@gmail.com>
2024-04-15 18:32:51 +0200

    Add required tag on user.profile.email

--- resources/views/profile/partials/update-profile-information-form.blade.php
+++ resources/views/profile/partials/update-profile-information-form.blade.php
@@ -43,9 +43,9 @@
         </div>
 
         <div>
-            <x-input-label for="email" :value="__('Email')"/>
+            <x-input-label for="email" :value="__('Email')" :required_tag="true"/>
             <x-text-input id="email" name="email" type="email" class="mt-1 block w-full"
-                          :value="old('email', $user->email)" required autocomplete="username"/>
+                          :value="old('email', $user->email)" required autocomplete="email"/>
             <x-input-error class="mt-2" :messages="$errors->get('email')"/>
 
             @if ($user instanceof \Illuminate\Contracts\Auth\MustVerifyEmail && ! $user->hasVerifiedEmail())
