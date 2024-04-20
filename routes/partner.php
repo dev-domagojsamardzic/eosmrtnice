d6b914ffbfd64fa79cd5b4c41dc2e342492904ed
dev-domagojsamardzic <dev.domagojsamardzic@gmail.com>
2024-04-20 20:17:42 +0200

    Register ads route, create nude AdController

--- routes/partner.php
+++ routes/partner.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Partner\AdController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\Partner\CompanyController;
@@ -9,6 +10,8 @@ Route::get('/dashboard', static function () {
 })->name('dashboard');
 
 Route::resource('companies', CompanyController::class)
+    ->only(['index', 'create', 'edit', 'update', 'store']);
+Route::resource('ads', AdController::class)
     ->only(['index', 'create', 'edit', 'update', 'store']);
 
 Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
