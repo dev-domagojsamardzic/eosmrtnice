54d268174ec9c4591c07815e2f8e60a50a8e9a13
dev-domagojsamardzic <dev.domagojsamardzic@gmail.com>
2024-04-20 20:18:06 +0200

    Create AdsTable livewire component

---
+++ app/Http/Livewire/Tables/Partner/AdsTable.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Livewire\Tables\Partner;
+
+use App\Models\Ad;
+use Filament\Forms\Concerns\InteractsWithForms;
+use Filament\Forms\Contracts\HasForms;
+use Filament\Tables;
+use Filament\Tables\Concerns\InteractsWithTable;
+use Filament\Tables\Contracts\HasTable;
+use Filament\Tables\Table;
+use Illuminate\Contracts\View\View;
+use Livewire\Component;
+
+class AdsTable extends Component implements HasForms, HasTable
+{
+    use InteractsWithForms;
+    use InteractsWithTable;
+
+    public function table(Table $table): Table
+    {
+        return $table
+            ->query(Ad::query())
+            ->columns([
+                //
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                //
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    //
+                ]),
+            ]);
+    }
+
+    public function render(): View
+    {
+        return view('livewire.ads-table');
+    }
+}
---
+++ resources/views/partner/ads/index.blade.php
@@ -0,0 +1,8 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-weight-bolder mb-5">
+            {{ __('models/ads.ads') }}
+        </h2>
+    </x-slot>
+    @livewire('tables.partner.ads-table')
+</x-app-layout>
