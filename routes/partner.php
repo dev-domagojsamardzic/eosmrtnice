41508bdb9c05fe316e30849345c27e7a4b202468
dev-domagojsamardzic <dev.domagojsamardzic@gmail.com>
2024-05-04 15:31:21 +0200

    Fix route param

--- routes/partner.php
+++ routes/partner.php
@@ -16,10 +16,10 @@ Route::get('ads', [AdController::class, 'index'])->name('ads.index');
 
 Route::prefix('companies/{company}')->group(function () {
     Route::get('ads/create', [AdController::class, 'create'])->name('ads.create');
-    Route::get('ads/{id}/edit', [AdController::class, 'edit'])->name('ads.edit');
+    Route::get('ads/{ad}/edit', [AdController::class, 'edit'])->name('ads.edit');
     Route::post('ads', [AdController::class, 'store'])->name('ads.store');
-    Route::put('ads/{id}', [AdController::class, 'update'])->name('ads.update');
-    Route::delete('ads/{id}', [AdController::class, 'destroy'])->name('ads.destroy');
+    Route::put('ads/{ad}', [AdController::class, 'update'])->name('ads.update');
+    Route::delete('ads/{ad}', [AdController::class, 'destroy'])->name('ads.destroy');
 });
 
 Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
