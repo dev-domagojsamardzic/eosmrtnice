ffc67d55e989c19ae6d6cb829d2d9f1adbb4be7c
dev-domagojsamardzic <dev.domagojsamardzic@gmail.com>
2024-05-04 19:19:04 +0200

    create public_storage_asset helper fn

--- app/helpers.php
+++ app/helpers.php
@@ -46,3 +46,15 @@ if (!function_exists('is_member')) {
         return auth()?->user()?->type === UserType::MEMBER;
     }
 }
+
+if (!function_exists('public_storage_asset')) {
+    /**
+     * Returns asset link for item in public/storage directory
+     * @param string $path
+     * @return string
+     */
+    function public_storage_asset(string $path = ''): string
+    {
+        return asset('storage/' . $path);
+    }
+}
