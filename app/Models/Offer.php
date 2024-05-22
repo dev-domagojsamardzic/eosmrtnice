9ee8b7387c2335f07e3989e22e6fa0b0e1754ee4
dev-domagojsamardzic <dev.domagojsamardzic@gmail.com>
2024-05-22 17:39:49 +0200

    Create valid scope

--- app/Models/Offer.php
+++ app/Models/Offer.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
@@ -50,5 +51,15 @@ class Offer extends Model
     public function offerables(): HasMany
     {
         return $this->hasMany(Offerable::class)->with('offerables');
+    }
+
+
+    /**
+     * Scope a query to only include valid offers
+     */
+    public function scopeValid(Builder $query): void
+    {
+        $query->where('valid_from', '<=', now()->toDateString())
+            ->where('valid_until', '>=', now()->toDateString());
     }
 }
