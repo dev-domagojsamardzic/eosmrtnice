d90c5cda9dc847be0ac8a82beb7b98c9f505a719
dev-domagojsamardzic <dev.domagojsamardzic@gmail.com>
2024-05-23 22:58:25 +0200

    Update PHPDoc

--- app/Models/Offerable.php
+++ app/Models/Offerable.php
@@ -6,11 +6,21 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\MorphTo;
 use Illuminate\Database\Eloquent\Relations\Pivot;
 
+/**
+ * @property-read       int         id
+ * @property            int         offer_id
+ * @property            string      offerable_type
+ * @property            int         offerable_id
+ * @property            int         quantity
+ * @property            float       price
+ */
 class Offerable extends Pivot
 {
     use HasFactory;
 
     protected $table = 'offerables';
+
+    public $timestamps = false;
 
     protected $attributes = [
         'quantity' => 1,
