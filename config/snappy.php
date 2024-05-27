35c3f6004ed783ede634de40fce843c25ccab8ee
dev-domagojsamardzic <dev.domagojsamardzic@gmail.com>
2024-05-27 22:58:34 +0200

    Configurate snappy pdf

---
+++ config/snappy.php
@@ -0,0 +1,61 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Snappy PDF / Image Configuration
+    |--------------------------------------------------------------------------
+    |
+    | This option contains settings for PDF generation.
+    |
+    | Enabled:
+    |
+    |    Whether to load PDF / Image generation.
+    |
+    | Binary:
+    |
+    |    The file path of the wkhtmltopdf / wkhtmltoimage executable.
+    |
+    | Timeout:
+    |
+    |    The amount of time to wait (in seconds) before PDF / Image generation is stopped.
+    |    Setting this to false disables the timeout (unlimited processing time).
+    |
+    | Options:
+    |
+    |    The wkhtmltopdf command options. These are passed directly to wkhtmltopdf.
+    |    See https://wkhtmltopdf.org/usage/wkhtmltopdf.txt for all options.
+    |
+    | Env:
+    |
+    |    The environment variables to set while running the wkhtmltopdf process.
+    |
+    */
+
+    'pdf' => [
+        'enabled' => true,
+        'binary'  => '/usr/local/bin/wkhtmltopdf',
+        'timeout' => true,
+        'options' => [
+            'enable-local-file-access' => true,
+            'encoding' => 'UTF-8',
+            'page-size' => 'A4',
+            'orientation' => 'portrait',
+            'margin-bottom' => 20,
+            'margin-top' => 20,
+            'margin-left' => 20,
+            'margin-right' => 20,
+        ],
+        'env'     => [],
+    ],
+
+    'image' => [
+        'enabled' => true,
+        'binary'  => env('WKHTML_IMG_BINARY', '/usr/local/bin/wkhtmltoimage'),
+        'timeout' => false,
+        'options' => [],
+        'env'     => [],
+    ],
+
+];
