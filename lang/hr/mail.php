3b9e9151f61e010914c6a40ae2c38e89945742b1
dev-domagojsamardzic <dev.domagojsamardzic@gmail.com>
2024-05-28 23:03:10 +0200

    Change mail subject

--- lang/hr/mail.php
+++ lang/hr/mail.php
@@ -5,7 +5,7 @@ return [
     'kind_regards' => 'S poštovanjem,',
     'team' => 'Team',
     'ad_created' => [
-        'subject' => 'Nova reklama',
+        'subject' => 'Novi oglas',
         'intro' => 'Obavještavamo Vas da je korisnik :owner napravio novu reklamu za tvrtku :company, u vrijeme :datetime.',
         'ad_properties' => 'Svojstva',
         'outro' => 'Pošaljite korisniku ponudu, a nakon uplate ne zaboravite odobriti oglas kako bi postao spreman za prikazivanje.',
