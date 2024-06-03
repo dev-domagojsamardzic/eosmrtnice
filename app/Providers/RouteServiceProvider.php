96252127d48552215798cb11b6dc7bc3c3bb4fce
dev-domagojsamardzic <dev.domagojsamardzic@gmail.com>
2024-06-03 21:37:27 +0200

    Change member route

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -19,7 +19,7 @@ class RouteServiceProvider extends ServiceProvider
      */
     public const ADMIN = '/admin/dashboard';
     public const PARTNER = '/partner/dashboard';
-    public const MEMBER = '/member/dashboard';
+    public const MEMBER = '/user/dashboard';
     public const HOME = '/';
 
     /**
