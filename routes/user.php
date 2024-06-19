e2306407e7edad0450c4b8aeb87ccdbc63383080
dev-domagojsamardzic <dev.domagojsamardzic@gmail.com>
2024-06-19 20:57:25 +0200

    Register edit and update routes

--- routes/user.php
+++ routes/user.php
@@ -16,6 +16,8 @@ Route::get('posts', [PostController::class, 'index'])->name('posts.index');
 Route::prefix('deceased/{deceased}')->group(function () {
     Route::get('posts/create', [PostController::class, 'create'])->name('posts.create');
     Route::post('posts', [PostController::class, 'store'])->name('posts.store');
+    Route::get('posts/{post}/edit', [PostController::class, 'edit'])->name('posts.edit');
+    Route::put('posts/{post}', [PostController::class, 'update'])->name('posts.update');
 });
 
 Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
