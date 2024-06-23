04f01387cf62ca47351f4b02890cbb3b1fc472a8
dev-domagojsamardzic <dev.domagojsamardzic@gmail.com>
2024-06-23 22:06:52 +0200

    Include posts in offer

--- resources/views/pdf/offer.blade.php
+++ resources/views/pdf/offer.blade.php
@@ -24,13 +24,24 @@
         <tbody>
         <tr class="mt-16">
             <td class="text-align-left">
-                <table>
-                    <tr><td><strong>{{ __('models/offer.buyer') }}:</strong></td></tr>
-                    <tr><td>{{ $offer->company->title }}</td></tr>
-                    <tr><td>{{ $offer->company->address }}</td></tr>
-                    <tr><td>{{ $offer->company->zipcode . ' ' . $offer->company->city->title }}</td></tr>
-                    <tr><td>{{ config('app.country') }}</td></tr>
-                </table>
+                @if(!is_null($offer->company))
+                    <table>
+                        <tr><td><strong>{{ __('models/offer.buyer') }}:</strong></td></tr>
+                        <tr><td>{{ $offer->company->title }}</td></tr>
+                        <tr><td>{{ $offer->company->address }}</td></tr>
+                        <tr><td>{{ $offer->company->zipcode . ' ' . $offer->company->city->title }}</td></tr>
+                        <tr><td>{{ config('app.country') }}</td></tr>
+                    </table>
+                @elseif(!is_null($offer->user))
+                    <table>
+                        <tr><td><strong>{{ __('models/offer.buyer') }}:</strong></td></tr>
+                        <tr><td>{{ $offer->user->full_name }}</td></tr>
+                        <tr><td>{{ $offer->user->email }}</td></tr>
+                        <tr><td>{{ config('app.country') }}</td></tr>
+                    </table>
+                @else
+
+                @endif
             </td>
             <td class="text-align-right">
                 <table class="w-100">
@@ -66,16 +77,35 @@
         </tr>
         </thead>
         <tbody>
-        @foreach($offer->ads as $item)
-            <tr>
-                <td class="text-align-left">{{ $item->title ?? $item->fallbackTitle }}</td>
-                <td class="text-align-right">{{ $item->pivot?->quantity }}</td>
-                <td class="text-align-right">{{ $item->pivot?->price . config('app.currency_symbol') }}</td>
-                <td class="text-align-right">
-                    {{ number_format((int)$item->pivot?->quantity * (float)$item->pivot?->price, 2) . config('app.currency_symbol') }}
-                </td>
-            </tr>
-        @endforeach
+        @if($offer->ads)
+            @foreach($offer->ads as $item)
+                <tr>
+                    <td class="text-align-left">{{ $item->title ?? $item->fallbackTitle }}</td>
+                    <td class="text-align-right">{{ $item->pivot?->quantity }}</td>
+                    <td class="text-align-right">{{ $item->pivot?->price . config('app.currency_symbol') }}</td>
+                    <td class="text-align-right">
+                        {{ number_format((int)$item->pivot?->quantity * (float)$item->pivot?->price, 2) . config('app.currency_symbol') }}
+                    </td>
+                </tr>
+            @endforeach
+        @endif
+        @if($offer->posts)
+            @foreach($offer->posts as $item)
+                <tr>
+                    <td class="text-align-left">
+                        {{ "{$item->type->translate()}: {$item->deceased_full_name_lg}" }} <br>
+                        {{ __('models/post.size').': '.ucfirst($item->size->name).' ('.__('common.to').' '.$item->size->value.' '.__('common.words').')' }} <br>
+                        {{ __('models/post.is_framed') }}: {{ $item->is_framed ? __('common.yes') : __('common.no') }}
+                    </td>
+                    <td class="text-align-right">{{ $item->pivot?->quantity }}</td>
+                    <td class="text-align-right">{{ $item->pivot?->price . config('app.currency_symbol') }}</td>
+                    <td class="text-align-right">
+                        {{ number_format((int)$item->pivot?->quantity * (float)$item->pivot?->price, 2) . config('app.currency_symbol') }}
+                    </td>
+                </tr>
+            @endforeach
+        @endif
+
 
         <tr>
             <td class="text-align-left" colspan="3"><strong>{{ __('models/offer.pdv') . ' ' . config('app.tax_percentage') . '%'}}</strong></td>
