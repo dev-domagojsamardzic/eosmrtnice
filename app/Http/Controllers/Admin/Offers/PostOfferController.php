0e540384d43311910fda4f0fb592dbce9b836ea0
dev-domagojsamardzic <dev.domagojsamardzic@gmail.com>
2024-06-23 19:51:38 +0200

    Create store and apply fns

--- app/Http/Controllers/Admin/Offers/PostOfferController.php
+++ app/Http/Controllers/Admin/Offers/PostOfferController.php
@@ -3,9 +3,14 @@
 namespace App\Http\Controllers\Admin\Offers;
 
 use App\Http\Controllers\Admin\OfferController;
+use App\Http\Requests\Admin\Offers\PostOfferRequest;
+use App\Mail\OfferCreated;
 use App\Models\Offer;
 use App\Models\Post;
+use Carbon\Carbon;
+use Exception;
 use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\View\View;
 
 class PostOfferController extends OfferController
@@ -19,6 +24,11 @@ class PostOfferController extends OfferController
     public function create(Post $post): View|RedirectResponse
     {
         return $this->form(new Offer, $post, 'create');
+    }
+
+    public function store(Offer $offer, PostOfferRequest $request): RedirectResponse
+    {
+        return $this->apply($offer, $request);
     }
 
     /**
@@ -47,5 +57,52 @@ class PostOfferController extends OfferController
                 'quit' => route(auth_user_type() . '.posts.index', ['post' => $post]),
             ]
         );
+    }
+
+    /**
+     * Apply changes on resource
+     * @param Offer $offer
+     * @param PostOfferRequest $request
+     * @return RedirectResponse
+     */
+    protected function apply(Offer $offer, PostOfferRequest $request): RedirectResponse
+    {
+        $offer->user()->associate($request->input('user_id'));
+        $total = (float)$request->input('quantity') * $request->input('price');
+        $taxes = (float)($total * (config('app.tax_percentage') / 100));
+        $offer->total = $total;
+        $offer->taxes = $taxes;
+        $offer->net_total = $total - $taxes;
+        $offer->valid_from = Carbon::parse($request->input('valid_from'))->format('Y-m-d');
+        $offer->valid_until = Carbon::parse($request->input('valid_until'))->format('Y-m-d');
+
+        try {
+            $offer->save();
+
+            $offer->offerables()->create([
+                'offerable_id' => $request->input('offerable_id'),
+                'offerable_type' => Post::class,
+                'quantity' => $request->input('quantity'),
+                'price' => $request->input('price'),
+            ]);
+
+            if ($request->submit === 'save_and_send') {
+
+                Mail::to($offer->company)->send(new OfferCreated($offer));
+
+                $offer->sent_at = now();
+                $offer->save();
+
+                return redirect()->route('admin.posts.index')
+                    ->with('alert', ['class' => 'success', 'message' => __('models/offer.messages.offer_sent')]);
+            }
+
+            return redirect()->route('admin.posts.index')
+                ->with('alert', ['class' => 'success', 'message' => __('common/saved')]);
+        } catch (Exception $e) {
+            return redirect()
+                ->route('admin.posts.index')
+                ->with('alert', ['class' => 'danger', 'message' => __('common.something_went_wrong')]);
+        }
     }
 }
