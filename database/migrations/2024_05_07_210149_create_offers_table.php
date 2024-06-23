ea97247afeea65580779776c10450de71e057856
dev-domagojsamardzic <dev.domagojsamardzic@gmail.com>
2024-06-23 19:49:54 +0200

    Update migration

--- database/migrations/2024_05_07_210149_create_offers_table.php
+++ database/migrations/2024_05_07_210149_create_offers_table.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Models\Company;
+use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -15,7 +16,8 @@ return new class extends Migration
         Schema::create('offers', static function (Blueprint $table) {
             $table->id();
             $table->string('number')->nullable(false);
-            $table->foreignIdFor(Company::class)->constrained();
+            $table->foreignIdFor(Company::class)->nullable()->constrained();
+            $table->foreignIdFor(User::class)->nullable()->constrained();
             $table->unsignedDecimal('net_total')->nullable(false);
             $table->unsignedDecimal('taxes')->nullable(false);
             $table->unsignedDecimal('total')->nullable(false);
