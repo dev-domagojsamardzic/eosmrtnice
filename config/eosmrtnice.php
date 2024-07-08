9875dea92729fb3ff1f472564de274d3d929d7a4
dev-domagojsamardzic <dev.domagojsamardzic@gmail.com>
2024-07-08 19:32:41 +0200

    Complement config values with env vars

--- config/eosmrtnice.php
+++ config/eosmrtnice.php
@@ -7,18 +7,18 @@ return [
      * *------------------------------------------------------------*
      */
     'company' => [
-        'title' => env('COMPANY_TITLE', 'eOsmrtnice d.o.o.'),
-        'address' => env('COMPANY_ADDRESS', 'Addresa Hrvatskih Branitelja 11'),
-        'zipcode' => env('COMPANY_ZIPCODE', '21300'),
-        'town' => env('COMPANY_TOWN', 'Grad'),
-        'country' => env('COMPANY_COUNTRY', 'Republika Hrvatska'),
-        'oib' => env('COMPANY_OIB', '11457658984'),
-        'email' => env('COMPANY_EMAIL', 'info@eosmrtnice.com'),
-        'phone' => env('COMPANY_PHONE', '+38523414999'),
-        'mobile_phone' => env('COMPANY_MOBILE_PHONE', '+385923419912'),
+        'title' => env('EOS_COMPANY_TITLE', 'eOsmrtnice d.o.o.'),
+        'address' => env('EOS_COMPANY_ADDRESS', 'Addresa Hrvatskih Branitelja 11'),
+        'zipcode' => env('EOS_COMPANY_ZIPCODE', '21300'),
+        'town' => env('EOS_COMPANY_TOWN', 'Grad'),
+        'country' => env('EOS_COMPANY_COUNTRY', 'Republika Hrvatska'),
+        'oib' => env('EOS_COMPANY_OIB', '11457658984'),
+        'email' => env('EOS_COMPANY_EMAIL', 'info@eosmrtnice.com'),
+        'phone' => env('EOS_COMPANY_PHONE', '+38523414999'),
+        'mobile_phone' => env('EOS_COMPANY_MOBILE_PHONE', '+385923419912'),
     ],
 
-    'mail_from_address' => env('MAIL_FROM_ADDRESS', 'info@eosmrtnice.com'),
+    'mail_from_address' => env('EOS_MAIL_FROM_ADDRESS', 'info@eosmrtnice.com'),
 
     /**
      * *-------------------------------*
@@ -26,8 +26,17 @@ return [
      * --------------------------------*
      */
     'image_dimensions' => [
-        'deceased_image' => ['width' => 220, 'height' => 280],
-        'company_logo' => ['width' => 120, 'height' => 120],
-        'ad_banner' => ['width' => 500, 'height' => 300],
+        'deceased_image' => [
+            'width' => env('EOS_DECEASED_IMAGE_WIDTH', 200),
+            'height' => env('EOS_DECEASED_IMAGE_HEIGHT', 280),
+        ],
+        'company_logo' => [
+            'width' => env('EOS_COMPANY_LOGO_WIDTH', 120),
+            'height' => env('EOS_COMPANY_LOGO_HEIGHT', 120),
+        ],
+        'ad_banner' => [
+            'width' => env('EOS_COMPANY_AD_BANNER_WIDTH', 500),
+            'height' => env('EOS_COMPANY_AD_BANNER_HEIGHT', 300),
+        ],
     ],
 ];
