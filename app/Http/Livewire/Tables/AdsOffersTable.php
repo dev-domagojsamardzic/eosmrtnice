39355af2307dc67958fd8d2f9e2ca36f6eef1497
dev-domagojsamardzic <dev.domagojsamardzic@gmail.com>
2024-07-10 22:56:46 +0200

    Extend AdsOffersTable.php

---
+++ app/Http/Livewire/Tables/AdsOffersTable.php
@@ -0,0 +1,134 @@
+<?php
+
+namespace App\Http\Livewire\Tables;
+
+use App\Models\AdsOffer;
+use Exception;
+use Filament\Forms\Concerns\InteractsWithForms;
+use Filament\Forms\Contracts\HasForms;
+use Filament\Support\Enums\FontWeight;
+use Filament\Support\Enums\MaxWidth;
+use Filament\Tables;
+use Filament\Tables\Columns\Layout\Panel;
+use Filament\Tables\Columns\Layout\Split;
+use Filament\Tables\Columns\Layout\Stack;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Columns\ViewColumn;
+use Filament\Tables\Concerns\InteractsWithTable;
+use Filament\Tables\Contracts\HasTable;
+use Filament\Tables\Enums\FiltersLayout;
+use Filament\Tables\Table;
+use Illuminate\Contracts\View\View;
+use Illuminate\Database\Eloquent\Builder;
+use Livewire\Component;
+
+class AdsOffersTable extends Component implements HasForms, HasTable
+{
+    use InteractsWithForms;
+    use InteractsWithTable;
+
+    /**
+     * @param Table $table
+     * @return Table
+     * @throws Exception
+     */
+    public function table(Table $table): Table
+    {
+        return $table
+            ->emptyStateHeading(__('common.no_records'))
+            ->emptyStateDescription('')
+            ->striped()
+            ->query($this->getQuery())
+            ->columns($this->getColumns())
+            ->filters($this->getFilters(), layout: FiltersLayout::AboveContent)
+            ->filtersFormColumns(4)
+            ->filtersFormWidth(MaxWidth::FourExtraLarge)
+            ->actions($this->getActions());
+    }
+
+    public function render(): View
+    {
+        return view('livewire.table');
+    }
+
+    protected function getQuery(): Builder
+    {
+        return AdsOffer::query();
+    }
+
+    /**
+     * Get table columns
+     *
+     * @return array
+     */
+    protected function getColumns(): array
+    {
+        return [
+            Split::make([
+                TextColumn::make('number')
+                    ->label(__('models/offer.number'))
+                    ->searchable()
+                    ->weight(FontWeight::Bold),
+                Stack::make([
+                    TextColumn::make('valid_from')
+                        ->label(__('models/offer.valid_from'))
+                        ->formatStateUsing(fn(AdsOffer $o):string => __('models/offer.valid_from').': ' . $o->valid_from->format('d.m.Y.') ),
+                    TextColumn::make('valid_until')
+                        ->label(__('models/offer.valid_until'))
+                        ->formatStateUsing(fn(AdsOffer $o):string => __('models/offer.valid_until').': '.$o->valid_until->format('d.m.Y.')),
+                ]),
+                ViewColumn::make('offer_sent')
+                    ->view('filament.tables.columns.offer-sent-badge'),
+                TextColumn::make('created_at')
+                    ->label(__('models/offer.created_at'))
+                    ->date('d.m.Y.')
+            ])->from('md'),
+            Panel::make([
+                Tables\Columns\Layout\Grid::make([
+                    'lg' => 2, 'sm' => 1,
+                ])->schema([
+                    Stack::make([
+                        TextColumn::make('company.title')
+                            ->label(__('models/offer.company'))
+                            ->searchable(),
+                        TextColumn::make('company.address')
+                            ->label(__('models/company.address')),
+                        TextColumn::make('company.town')
+                            ->label(__('models/offer.town'))
+                            ->formatStateUsing(fn(AdsOffer $o):string => $o->company->zipcode.', '.$o->company->town)
+                            ->searchable(),
+                        TextColumn::make('company.oib')
+                            ->label(__('models/offer.oib')),
+                    ]),
+                    Stack::make([
+                        TextColumn::make('taxes')
+                            ->formatStateUsing(fn(AdsOffer $o): string => __('models/offer.taxes').': '.$o->taxes.config('app.currency_symbol')),
+                        TextColumn::make('total')
+                            ->formatStateUsing(fn(AdsOffer $o): string => __('models/offer.total').': '.$o->total.config('app.currency_symbol')),
+                    ]),
+                ])
+
+            ])->collapsible()->columnSpanFull(),
+        ];
+    }
+
+    /**
+     * Return table filters
+     *
+     * @throws Exception
+     */
+    protected function getFilters(): array
+    {
+        return [];
+    }
+
+    /**
+     * Get table actions
+     *
+     * @return array
+     */
+    protected function getActions(): array
+    {
+        return [];
+    }
+}
--- app/Http/Livewire/Tables/Partner/AdsOffersTable.php
+++ app/Http/Livewire/Tables/Partner/AdsOffersTable.php
@@ -1,30 +1,30 @@
 <?php
 
-namespace App\Http\Livewire\Tables\Admin;
+namespace App\Http\Livewire\Tables\Partner;
 
-use App\Http\Controllers\Admin\AdsOfferController;
 use App\Models\AdsOffer;
+use App\Models\Company;
 use Exception;
 use Filament\Forms\Components\DatePicker;
 use Filament\Support\Enums\IconPosition;
 use Filament\Tables\Actions\Action;
 use Filament\Tables\Actions\ActionGroup;
-use Filament\Tables\Actions\DeleteAction;
-use Filament\Tables\Actions\EditAction;
 use Filament\Tables\Filters\Filter;
-use Filament\Tables\Filters\TernaryFilter;
 use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Http\RedirectResponse;
 use Illuminate\Support\Carbon;
-use Livewire\Features\SupportRedirects\Redirector;
 use App\Http\Livewire\Tables\AdsOffersTable as BaseTable;
 
 class AdsOffersTable extends BaseTable
 {
     protected function getQuery(): Builder
     {
-        return AdsOffer::query()->orderBy('created_at');
+        $companies = Company::query()->where('user_id', auth()->id())->pluck('id')->toArray();
+
+        return AdsOffer::query()
+            ->whereIn('company_id', $companies)
+            ->orderBy('created_at');
     }
+
 
     /**
      * Return table filters
@@ -78,11 +78,6 @@ class AdsOffersTable extends BaseTable
                             fn (Builder $query, $date): Builder => $query->whereDate('valid_until', '<=', $date),
                         );
                 }),
-            TernaryFilter::make('sent_at')
-                ->label(__('models/offer.sent_at'))
-                ->nullable()
-                ->trueLabel(__('models/offer.sent_offers'))
-                ->falseLabel(__('models/offer.not_sent_offers'))
         ];
     }
 
@@ -95,26 +90,10 @@ class AdsOffersTable extends BaseTable
     {
         return [
             ActionGroup::make([
-                Action::make('send_offer')
-                    ->label(__('models/offer.send'))
-                    ->icon('heroicon-o-paper-airplane')
-                    ->visible(fn(AdsOffer $ads_offer): bool => is_null($ads_offer->sent_at))
-                    ->url(fn(AdsOffer $ads_offer): string => route(auth_user_type() . '.ads-offers.send', ['ads_offer' => $ads_offer->id])),
                 Action::make('download_offer_pdf')
                     ->label(__('models/offer.download_pdf'))
                     ->icon('heroicon-o-arrow-down-tray')
                     ->url(fn(AdsOffer $ads_offer) => route(auth_user_type() . '.ads-offers.download', ['ads_offer' => $ads_offer->id])),
-                EditAction::make('view')
-                    ->label(__('common.edit'))
-                    ->icon('heroicon-s-pencil-square')
-                    ->url(fn(AdsOffer $ads_offer) => route(auth_user_type() . '.ads-offers.edit', $ads_offer)),
-                DeleteAction::make('delete')
-                    ->label(__('common.delete'))
-                    ->icon('heroicon-s-trash')
-                    ->requiresConfirmation()
-                    ->modalHeading(__(auth_user_type() . '.delete_offer'))
-                    ->modalSubmitActionLabel(__('common.delete'))
-                    ->action(fn (AdsOffer $ads_offer): RedirectResponse|Redirector => (new AdsOfferController())->destroy($ads_offer))
             ])->iconPosition(IconPosition::Before),
         ];
     }
