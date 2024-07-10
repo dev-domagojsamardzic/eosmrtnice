f07871293c395694f1e56675278db63ca8915d80
dev-domagojsamardzic <dev.domagojsamardzic@gmail.com>
2024-07-10 17:24:51 +0200

    Delete excess tables and seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,6 @@ class DatabaseSeeder extends Seeder
         $this->call([
             UserSeeder::class,
             CompanySeeder::class,
-            ServiceSeeder::class,
         ]);
     }
 }
