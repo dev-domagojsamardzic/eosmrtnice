58c84318e3bbac1c039c57a2a3a840ed4fa0875e
dev-domagojsamardzic <dev.domagojsamardzic@gmail.com>
2024-07-10 23:37:02 +0200

    Delete unnecessary blades

--- resources/views/admin/posts/index.blade.php
+++
@@ -1,8 +0,0 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-gray-800 mb-4 dark:text-gray-200">
-            {{ __('models/post.post') }}
-        </h2>
-    </x-slot>
-    @livewire('tables.admin.posts-table')
-</x-app-layout>
