ff24ea57d5af2bc72e699dcf8715ab7bcbc523bf
dev-domagojsamardzic <dev.domagojsamardzic@gmail.com>
2024-07-11 23:20:34 +0200

    Create offers table, Offer model, extend Offer model

---
+++ app/Models/Offer.php
@@ -0,0 +1,145 @@
+<?php
+
+namespace App\Models;
+
+use Barryvdh\Snappy\Facades\SnappyPdf;
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Casts\Attribute;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Http\Response;
+
+/**
+ * @property-read       int                 id
+ * @property            int                 company_id
+ * @property            int                 ad_id
+ * @property            int                 user_id
+ * @property            int                 post_id
+ * @property            string              number
+ * @property            int                 quantity
+ * @property            float               price
+ * @property            float               net_total
+ * @property            float               taxes
+ * @property            float               total
+ * @property            Carbon              valid_from
+ * @property            Carbon              valid_until
+ * @property            bool                confirmed
+ * @property            Carbon              sent_at
+ * @property            Carbon              created_at
+ * @property            Carbon              updated_at
+ * @property            Carbon              deleted_at
+ *  ------------------------------------------------------------
+ * @property-read       bool                is_valid
+ *  ------------------------------------------------------------
+ * @property            Company             company
+ * @property            Ad                  ad
+ * @property            User                user
+ * @property            Post                post
+ */
+class Offer extends Model
+{
+    use SoftDeletes;
+
+    protected $table = 'offers';
+
+    protected string $pdfView = 'pdf.offers';
+
+    protected $casts = [
+        'valid_from' => 'datetime',
+        'valid_until' => 'datetime',
+        'sent_at' => 'datetime',
+        'quantity' => 'integer',
+        'price' => 'decimal:2',
+        'net_total' => 'decimal:2',
+        'taxes' => 'decimal:2',
+        'total' => 'decimal:2',
+    ];
+
+    protected $attributes = [
+        'number' => 'P',
+        'confirmed' => false,
+    ];
+
+    /**
+     * Return user related to offer
+     * @return BelongsTo
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    /**
+     * Return post related to offer
+     * @return BelongsTo
+     */
+    public function post(): BelongsTo
+    {
+        return $this->belongsTo(Post::class);
+    }
+
+    /**
+     * Return company related to offer
+     * @return BelongsTo
+     */
+    public function company(): BelongsTo
+    {
+        return $this->belongsTo(Company::class);
+    }
+
+    /**
+     * Return company related to offer
+     * @return BelongsTo
+     */
+    public function ad(): BelongsTo
+    {
+        return $this->belongsTo(Ad::class);
+    }
+
+    /**
+     * Scope a query to only include valid offers
+     */
+    public function scopeValid(Builder $query): void
+    {
+        $query->where('valid_from', '<=', now()->toDateString())
+            ->where('valid_until', '>=', now()->toDateString());
+    }
+
+    /**
+     * Is offer valid
+     */
+    protected function isValid(): Attribute
+    {
+        return Attribute::make(
+            get: fn () => $this->valid_from->startOfDay()->lt(now()) &&
+                $this->valid_until->endOfDay()->gt(now())
+        );
+    }
+
+    /**
+     * Transform offer to raw pdf
+     *
+     * @return string
+     */
+    public function toRawPdf(): string
+    {
+        return SnappyPdf::loadView($this->pdfView, ['offer' => $this])
+            ->setOption('encoding', 'UTF-8')
+            ->output();
+    }
+
+    /**
+     * Return offer pdf as response
+     *
+     * @return Response
+     */
+    public function downloadPdf(): Response
+    {
+        return SnappyPdf::loadView($this->pdfView, ['offer' => $this])
+            ->setOption('encoding', 'UTF-8')
+            ->download($this->number . '.pdf');
+    }
+
+}
--- app/Models/PostsOffer.php
+++ app/Models/PostsOffer.php
@@ -2,119 +2,18 @@
 
 namespace App\Models;
 
-use Barryvdh\Snappy\Facades\SnappyPdf;
-use Carbon\Carbon;
-use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Database\Eloquent\Casts\Attribute;
-use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Illuminate\Database\Eloquent\SoftDeletes;
-use Illuminate\Http\Response;
+use App\Models\Scopes\AdsOfferScope;
+use App\Models\Scopes\PostsOfferScope;
 
-/**
- * @property-read       int                 id
- * @property            int                 user_id
- * @property            int                 post_id
- * @property            string              number
- * @property            int                 quantity
- * @property            float               price
- * @property            float               net_total
- * @property            float               taxes
- * @property            float               total
- * @property            Carbon              valid_from
- * @property            Carbon              valid_until
- * @property            bool                confirmed
- * @property            Carbon              sent_at
- * @property            Carbon              created_at
- * @property            Carbon              updated_at
- * @property            Carbon              deleted_at
- *  ------------------------------------------------------------
- * @property-read       bool                is_valid
- *  ------------------------------------------------------------
- * @property            User                user
- * @property            Post                post
- */
-class PostsOffer extends Model
+class PostsOffer extends Offer
 {
-    use SoftDeletes;
-
-    protected $table = 'posts_offers';
-
-    protected $casts = [
-        'valid_from' => 'datetime',
-        'valid_until' => 'datetime',
-        'sent_at' => 'datetime',
-        'quantity' => 'integer',
-        'price' => 'decimal:2',
-        'net_total' => 'decimal:2',
-        'taxes' => 'decimal:2',
-        'total' => 'decimal:2',
-    ];
-
-    protected $attributes = [
-        'number' => 'P',
-        'confirmed' => false,
-    ];
+    protected string $pdfView = 'pdf.posts-offer';
 
     /**
-     * Return user related to offer
-     * @return BelongsTo
+     * The "booted" method of the model.
      */
-    public function user(): BelongsTo
+    protected static function booted(): void
     {
-        return $this->belongsTo(User::class);
-    }
-
-    /**
-     * Return post related to offer
-     * @return BelongsTo
-     */
-    public function post(): BelongsTo
-    {
-        return $this->belongsTo(Post::class);
-    }
-
-    /**
-     * Scope a query to only include valid offers
-     */
-    public function scopeValid(Builder $query): void
-    {
-        $query->where('valid_from', '<=', now()->toDateString())
-            ->where('valid_until', '>=', now()->toDateString());
-    }
-
-    /**
-     * Is offer valid
-     */
-    protected function isValid(): Attribute
-    {
-        return Attribute::make(
-            get: fn () => $this->valid_from->startOfDay()->lt(now()) &&
-                $this->valid_until->endOfDay()->gt(now())
-        );
-    }
-
-    /**
-     * Transform offer to raw pdf
-     *
-     * @return string
-     */
-    public function toRawPdf(): string
-    {
-        return SnappyPdf::loadView('pdf.posts-offer', ['offer' => $this])
-            ->setOption('encoding', 'UTF-8')
-            ->output();
-    }
-
-    /**
-     * Return offer pdf as response
-     *
-     * @return Response
-     */
-    public function downloadPdf(): Response
-    {
-        return SnappyPdf::loadView('pdf.posts-offer', ['offer' => $this])
-            ->setOption('encoding', 'UTF-8')
-            ->download($this->number . '.pdf');
+        static::addGlobalScope(new PostsOfferScope);
     }
 }
