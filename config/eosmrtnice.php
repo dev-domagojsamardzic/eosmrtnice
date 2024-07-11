3b086e7472d2a8995ca08a581203bfce865352d1
dev-domagojsamardzic <dev.domagojsamardzic@gmail.com>
2024-07-11 22:44:47 +0200

    Add company and bank info in config

--- config/eosmrtnice.php
+++ config/eosmrtnice.php
@@ -7,15 +7,25 @@ return [
      * *------------------------------------------------------------*
      */
     'company' => [
-        'title' => env('EOS_COMPANY_TITLE', 'eOsmrtnice d.o.o.'),
-        'address' => env('EOS_COMPANY_ADDRESS', 'Addresa Hrvatskih Branitelja 11'),
-        'zipcode' => env('EOS_COMPANY_ZIPCODE', '21300'),
-        'town' => env('EOS_COMPANY_TOWN', 'Grad'),
+        'title' => env('EOS_COMPANY_TITLE', 'PROVEXIO d.o.o.'),
+        'address' => env('EOS_COMPANY_ADDRESS', 'Vinogradarska 3'),
+        'zipcode' => env('EOS_COMPANY_ZIPCODE', '40313'),
+        'town' => env('EOS_COMPANY_TOWN', 'Sv. Martin na Muri'),
         'country' => env('EOS_COMPANY_COUNTRY', 'Republika Hrvatska'),
-        'oib' => env('EOS_COMPANY_OIB', '11457658984'),
-        'email' => env('EOS_COMPANY_EMAIL', 'info@eosmrtnice.com'),
-        'phone' => env('EOS_COMPANY_PHONE', '+38523414999'),
-        'mobile_phone' => env('EOS_COMPANY_MOBILE_PHONE', '+385923419912'),
+        'oib' => env('EOS_COMPANY_OIB', '02540522475'),
+        'email' => env('EOS_COMPANY_EMAIL', 'info@provexio.hr'),
+        'phone' => env('EOS_COMPANY_PHONE', '040/886-219'),
+        'fax' => env('EOS_COMPANY_FAX', '040/886-219'),
+        'mobile_phone' => env('EOS_COMPANY_MOBILE_PHONE', ''),
+        'director' => env('EOS_COMPANY_DIRECTOR', 'Leon Rešetar'),
+    ],
+
+    'bank' => [
+        'title' => env('EOS_BANK_TITLE', 'Raiffeisen Bank d.d.'),
+        'iban' => env('EOS_BANK_IBAN', 'HR5524840081135274361'),
+        'swift_code' => env('EOS_BANK_SWIFT_CODE', 'RZBHHR2X'),
+        'model' => env('EOS_BANK_MODEL', '05'),
+        'reference_number' => env('EOS_BANK_REFNUMBER', '2410-0387'),
     ],
 
     'mail_from_address' => env('EOS_MAIL_FROM_ADDRESS', 'info@eosmrtnice.com'),
