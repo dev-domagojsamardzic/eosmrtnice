24d7c9601d86e82411278068ec1bf86852606417
dev-domagojsamardzic <dev.domagojsamardzic@gmail.com>
2024-07-13 14:00:15 +0200

    Edit title style and add some strikethrough

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -1,7 +1,7 @@
 <x-guest-layout>
     <div class="posts_wrapper">
         @foreach($posts as $dateString => $dateCollection)
-            <div class="posts_date_title w-100 text-center"><h1>{{ $dateString }}</h1></div>
+            <div class="posts_date_title w-100 text-center"><h1 class="font-weight-normal line-through">{{ $dateString }}</h1></div>
             <div class="masonry row posts_chunk_wrapper" data-masonry='{"percentPosition": true}'>
                 @foreach($dateCollection as $post)
                     <div class="post_wrapper col-sm-12 col-md-4">
