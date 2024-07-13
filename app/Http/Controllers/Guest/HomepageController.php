1b23eeb93d0ef70fa6e9bb4c641b044c6faf58fb
dev-domagojsamardzic <dev.domagojsamardzic@gmail.com>
2024-07-13 16:34:19 +0200

    Fetch 2 latest dates and load

--- app/Http/Controllers/Guest/HomepageController.php
+++ app/Http/Controllers/Guest/HomepageController.php
@@ -4,13 +4,26 @@ namespace App\Http\Controllers\Guest;
 
 use App\Http\Controllers\Controller;
 use App\Models\Post;
+use Illuminate\Support\Facades\DB;
 use Illuminate\View\View;
 
 class HomepageController extends Controller
 {
     public function home(): View
     {
+        // First loading, only show posts from 2 latest dates inserted
+        $latestDates = DB::table('posts')
+            ->select('starts_at')
+            ->distinct()
+            ->orderBy('starts_at', 'desc')
+            ->limit(2)
+            ->pluck('starts_at')
+            ->toArray();
+
         $posts = Post::query()
+            ->forDisplay()
+            ->todayOrOlder()
+            ->whereIn('starts_at', $latestDates)
             ->orderByDesc('starts_at')
             ->inRandomOrder()
             ->get()
