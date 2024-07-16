16b1b0390032c8f60c5621aa7561a5e8f33163d8
dev-domagojsamardzic <dev.domagojsamardzic@gmail.com>
2024-07-16 22:18:50 +0200

    Translate guest route URI-s

--- routes/web.php
+++ routes/web.php
@@ -55,26 +55,26 @@ Route::post('zahvale', [ThankYouController::class, 'items'])->name('guest.thank-
 
 Route::middleware('guest')->group(function () {
     // user register
-    Route::get('register', [RegisteredUserController::class, 'create'])
+    Route::get('registracija', [RegisteredUserController::class, 'create'])
         ->name('register');
-    Route::post('register', [RegisteredUserController::class, 'store'])
+    Route::post('registracija', [RegisteredUserController::class, 'store'])
         ->name('register');
 
     // partner register
-    Route::get('partner/register', [PartnerRegisteredUserController::class, 'create'])
+    Route::get('partner/registracija', [PartnerRegisteredUserController::class, 'create'])
         ->name('partner.register');
-    Route::post('partner/register', [PartnerRegisteredUserController::class, 'store'])
+    Route::post('partner/registracija', [PartnerRegisteredUserController::class, 'store'])
         ->name('partner.register');
 
-    Route::get('login', [AuthenticatedSessionController::class, 'create'])
+    Route::get('prijava', [AuthenticatedSessionController::class, 'create'])
         ->name('login');
 
-    Route::post('login', [AuthenticatedSessionController::class, 'store']);
+    Route::post('prijava', [AuthenticatedSessionController::class, 'store']);
 
-    Route::get('forgot-password', [PasswordResetLinkController::class, 'create'])
+    Route::get('zaboravljena-lozinka', [PasswordResetLinkController::class, 'create'])
         ->name('password.request');
 
-    Route::post('forgot-password', [PasswordResetLinkController::class, 'store'])
+    Route::post('zaboravljena-lozinka', [PasswordResetLinkController::class, 'store'])
         ->name('password.email');
 
     Route::get('reset-password/{token}', [NewPasswordController::class, 'create'])
@@ -85,25 +85,25 @@ Route::middleware('guest')->group(function () {
 });
 
 Route::middleware('auth')->group(function () {
-    Route::get('verify-email', EmailVerificationPromptController::class)
+    Route::get('email-verifikacija', EmailVerificationPromptController::class)
         ->name('verification.notice');
 
-    Route::get('verify-email/{id}/{hash}', VerifyEmailController::class)
+    Route::get('email-potvrda/{id}/{hash}', VerifyEmailController::class)
         ->middleware(['signed', 'throttle:6,1'])
         ->name('verification.verify');
 
-    Route::post('email/verification-notification', [EmailVerificationNotificationController::class, 'store'])
+    Route::post('email/potvrdna-obavijest', [EmailVerificationNotificationController::class, 'store'])
         ->middleware('throttle:6,1')
         ->name('verification.send');
 
-    Route::get('confirm-password', [ConfirmablePasswordController::class, 'show'])
+    Route::get('potvrda-lozinke', [ConfirmablePasswordController::class, 'show'])
         ->name('password.confirm');
 
-    Route::post('confirm-password', [ConfirmablePasswordController::class, 'store']);
+    Route::post('potvrda-lozinke', [ConfirmablePasswordController::class, 'store']);
 
-    Route::put('password', [PasswordController::class, 'update'])->name('password.update');
+    Route::put('lozinka', [PasswordController::class, 'update'])->name('password.update');
 
-    Route::post('logout', [AuthenticatedSessionController::class, 'destroy'])
+    Route::post('odjava', [AuthenticatedSessionController::class, 'destroy'])
         ->name('logout');
 
     // ImageController
