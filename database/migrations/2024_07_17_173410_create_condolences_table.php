267cf4698da8d21ec4c151cc04e626865a969f98
dev-domagojsamardzic <dev.domagojsamardzic@gmail.com>
2024-07-17 18:02:59 +0200

    Create condolences table

---
+++ database/migrations/2024_07_17_173410_create_condolences_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('condolences', static function (Blueprint $table) {
+            $table->id();
+
+            $table->char('motive',30);
+            $table->string('message', 2048)->nullable(false);
+
+            $table->string('recipient_full_name', 256)->nullable(false);
+            $table->string('recipient_address', 1024)->nullable(false);
+
+            $table->string('sender_full_name', 256)->nullable(false);
+            $table->string('sender_email', 128)->nullable(false);
+            $table->string('sender_phone', 64)->nullable(false);
+            $table->string('sender_address', 1024)->nullable(false);
+
+
+            $table->unsignedInteger('package_addon');
+
+            $table->string('recipient_full_name', 256)->nullable(false);
+            $table->string('recipient_address', 1024)->nullable(false);
+
+            $table->dateTime('paid_at');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('condolences');
+    }
+};
