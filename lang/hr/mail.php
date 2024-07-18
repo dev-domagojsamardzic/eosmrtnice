fa1acd5595f4a5e45123a2da40a3fafa1d37639e
dev-domagojsamardzic <dev.domagojsamardzic@gmail.com>
2024-07-18 18:05:52 +0200

    Mail translations

--- lang/hr/mail.php
+++ lang/hr/mail.php
@@ -24,4 +24,26 @@ return [
         'payment' => 'Nakon što platite, usluga će Vam se aktivirati.',
         'contact_info' => 'Za bilo kakva pitanja, molimo Vas da kontaktirate naš tim.'
     ],
+    'condolence_order_received' => [
+        'subject' => 'Zaprimljena narudžba za paket sućuti - broj :number',
+        'greetings' => 'Poštovani',
+        'intro' => 'Zaprimili smo Vašu narudžbu za paket sućuti, broj :number.',
+        'data_intro' => 'U nastavku se nalaze svi detalji Vaše narudžbe. Molimo Vas da detaljno pregledate narudžbu.',
+        'data_intro_contact' => 'Ukoliko otkrijete grešku u podatcima, obratite nam se putem maila :mail, ili na broj telefona :phone',
+        'order_info' => 'Podatci o narudžbi',
+        'recipient_data' => 'Podatci o primatelju',
+        'sender_data' => 'Podatci o naručitelju',
+        'payment' => 'Uskoro ćete zaprimiti mail sa ponudom za poslanu narudžbu.',
+        'kind_regards' => 'Hvala Vam na ukazanom povjerenju',
+    ],
+    'condolence_order_created' => [
+        'subject' => 'Zaprimljena narudžba za paket sućuti - broj :number',
+        'greetings' => 'Poštovani',
+        'intro' => 'Zaprimili smo novu narudžbu za paket sućuti, broj :number.',
+        'order_info' => 'Podatci o narudžbi',
+        'recipient_data' => 'Podatci o primatelju',
+        'sender_data' => 'Podatci o naručitelju',
+        'outro' => 'Kreirajte narudžbu za uslugu.',
+        'kind_regards' => 'Hvala Vam na ukazanom povjerenju',
+    ],
 ];
