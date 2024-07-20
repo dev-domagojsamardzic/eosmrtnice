a4f90283e2651e7ad41ed241db59a67f64bd7d96
dev-domagojsamardzic <dev.domagojsamardzic@gmail.com>
2024-07-20 17:32:20 +0200

    Create CondolencesOfferScope.php

---
+++ app/Models/Scopes/CondolencesOfferScope.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models\Scopes;
+
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Scope;
+
+class CondolencesOfferScope implements Scope
+{
+    /**
+     * Apply the scope to a given Eloquent query builder.
+     */
+    public function apply(Builder $builder, Model $model): void
+    {
+        $builder->whereNotNull('condolence_id')
+            ->whereNull('company_id')
+            ->whereNull('ad_id');
+    }
+}
