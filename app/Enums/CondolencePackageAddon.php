6aa9c69936c0da0186c7ca6b1ae320671f6ea382
dev-domagojsamardzic <dev.domagojsamardzic@gmail.com>
2024-09-18 23:36:43 +0200

    Change candle packages

--- app/Enums/CondolencePackageAddon.php
+++ app/Enums/CondolencePackageAddon.php
@@ -4,13 +4,14 @@ namespace App\Enums;
 
 enum CondolencePackageAddon: int
 {
-    case LANTERNS_6 = 1;
+    case LANTERNS_3 = 1;
     case LANTERNS_5 = 2;
-    case LANTERNS_4 = 3;
-    case LANTERNS_3 = 4;
-    case ANGEL_STATUE = 5;
-    case RED_ROSES = 6;
-    case WHITE_ROSES = 7;
+    case LANTERNS_6 = 3;
+    case LANTERNS_7 = 4;
+    case LANTERNS_10 = 5;
+    case ANGEL_STATUE = 6;
+    case RED_ROSES = 7;
+    case WHITE_ROSES = 8;
 
     public static function options(): array
     {
