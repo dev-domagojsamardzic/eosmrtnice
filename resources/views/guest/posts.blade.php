5b33a890075cd5a63158b46d8bdb72fc3d00d9f1
dev-domagojsamardzic <dev.domagojsamardzic@gmail.com>
2024-09-25 18:28:24 +0200

    Center empty state on homepage

--- resources/views/guest/posts.blade.php
+++ resources/views/guest/posts.blade.php
@@ -7,7 +7,7 @@
 
     <div class="row posts_wrapper" id="postsWrapper" data-masonry='{ "percentPosition": true, "itemSelector": ".masonry-item", "columnWidth": ".col-md-4" }'>
         @if($posts->isEmpty())
-            <div class="col-12 my-5 flex flex-col align-items-start justify-content-center">
+            <div class="col-12 my-5 flex flex-col align-items-center justify-content-center">
                 <i class="far fa-address-card text-gray-600" style="font-size: 62px;"></i>
                 <span class="text-gray-600 mt-4">{{ __('models/post.no_posts_yet') }}</span>
             </div>
