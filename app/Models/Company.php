a0dcfaf66ef0b847c382ecdb7876121cd4da3bb2
dev-domagojsamardzic <dev.domagojsamardzic@gmail.com>
2024-09-30 19:30:18 +0200

    Remove type, county, county, logo and website from company model

--- app/Models/Company.php
+++ app/Models/Company.php
@@ -2,46 +2,32 @@
 
 namespace App\Models;
 
-use App\Enums\CompanyType;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Support\Carbon;
 use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Database\Eloquent\Casts\Attribute;
 
 /**
  * @property        int         id
- * @property        int         type
  * @property        int         user_id
  * @property        string      title
  * @property        string      address
  * @property        string      town
  * @property        string      zipcode
- * @property        int         city_id
- * @property        int         county_id
  * @property        string      oib
  * @property        string      email
  * @property        string      phone
  * @property        string      mobile_phone
- * @property        string      website
- * @property        string      logo
  * @property        string      active
  * @property        Carbon      created_at
  * @property        Carbon      updated_at
  * @property        Carbon      deleted_at
  * -------------------------------------------
  * @method          static      active(Builder $query)
- * @method          static      availableForAd(Builder $query)
- * -------------------------------------------
- * @property        string      alternative_logo
  * -------------------------------------------
  * @property        Partner     user
- * @property        County      county
- * @property        City        city
- * @property        Ad          ads
  */
 
 class Company extends Model
@@ -64,7 +50,6 @@ class Company extends Model
      * @var array<string, string>
      */
     protected $casts = [
-        'type' => CompanyType::class,
         'created_at' => 'datetime',
         'updated_at' => 'datetime',
     ];
@@ -86,69 +71,10 @@ class Company extends Model
     }
 
     /**
-     * Company's county
-     * @return BelongsTo
-     */
-    public function county(): BelongsTo
-    {
-        return $this->belongsTo(County::class);
-    }
-
-    /**
-     * Company's city
-     * @return BelongsTo
-     */
-    public function city(): BelongsTo
-    {
-        return $this->belongsTo(City::class);
-    }
-
-
-    /**
-     * IMPORTANT!
-     * -----------------------------------------
-     * Although relationship is one to one, keep the name in plural (ads)
-     * because of implicit scope binding
-     * -----------------------------------------
-     * Ad that belongs to Company
-     * @return HasOne
-     */
-    public function ads(): HasOne
-    {
-        return $this->hasOne(Ad::class);
-    }
-
-    /**
      * Scope a query to only include active companies.
      */
     public function scopeActive(Builder $query): void
     {
         $query->where('active', 1);
-    }
-
-    /**
-     * Scope a query to only include companies available for ads
-     */
-    public function scopeAvailableForAd(Builder $query): void
-    {
-        $query->whereDoesntHave('ads', static function ($query) {
-            $query->where('expired', false);
-        });
-    }
-
-
-    /**
-     * Get alternative logo based on company type
-     */
-    protected function alternativeLogo(): Attribute
-    {
-        return Attribute::make(
-            get: fn() => match($this->type) {
-                CompanyType::FUNERAL => 'graphics/company_symbol/coffin-outline.svg',
-                CompanyType::MASONRY => 'graphics/company_symbol/tomb-outline.svg',
-                CompanyType::FLOWERS => 'graphics/company_symbol/flowers-outline.svg',
-                default => 'graphics/company_symbol/cross-outline.svg',
-            },
-        );
     }
 }
--- database/migrations/2024_03_05_215055_create_companies_table.php
+++ database/migrations/2024_03_05_215055_create_companies_table.php
@@ -1,7 +1,5 @@
 <?php
 
-use App\Models\City;
-use App\Models\County;
 use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
@@ -17,19 +15,14 @@ return new class extends Migration
         Schema::create('companies', static function (Blueprint $table) {
             $table->id();
             $table->foreignIdFor(User::class);
-            $table->string('logo')->nullable();
-            $table->unsignedTinyInteger('type')->nullable(false);
             $table->string('title')->nullable(false);
             $table->string('address')->nullable();
             $table->string('town')->nullable();
             $table->string('zipcode', 5)->nullable();
-            $table->foreignIdFor(City::class);
-            $table->foreignIdFor(County::class);
             $table->char('oib',11)->nullable(false);
             $table->string('email')->nullable();
             $table->string('phone')->nullable();
             $table->string('mobile_phone')->nullable();
-            $table->string('website')->nullable();
             $table->boolean('active')->default(true);
             $table->timestamps();
             $table->softDeletes();
