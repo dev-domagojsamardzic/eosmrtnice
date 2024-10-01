5faf236ad4f4e1620f63271d292bcc8768c2bc1b
dev-domagojsamardzic <dev.domagojsamardzic@gmail.com>
2024-10-01 23:06:52 +0200

    Update ads.form with new fields

--- resources/views/partner/ads/form.blade.php
+++ resources/views/partner/ads/form.blade.php
@@ -75,9 +75,12 @@
                 </div>
             @endcan
 
-            {{-- Title --}}
+            <h4>{{ __('models/ad.ad_data') }}</h4>
+            <hr>
+
             <div class="form-group row">
-                <div class="col-lg-6 col-md-12 mb-3">
+                {{-- Title --}}
+                <div class="col-lg-6 col-md-12">
                     <x-input-label for="title" :value="__('models/ad.title')"></x-input-label>
                     <x-input-info :content="__('models/ad.title_helper_info')" />
                     <x-text-input id="title"
@@ -87,43 +90,8 @@
                     />
                     <x-input-error :messages="$errors->get('title')" class="mt-2" />
                 </div>
-            </div>
-
-            {{-- Logo --}}
-            <div class="form-group row" id="form-panel-logo">
-                <div class="col-lg-6 col-md-12 mb-3">
-                    <x-input-label for="logo" :value="__('models/company.logo')"></x-input-label>
-                    <x-input-info :content="__('models/company.logo_helper_info')" />
-                    <input type="file" name="logo" id="logo">
-                    <small id="logo-message" class="text-xs font-weight-bold"></small>
-                    <x-input-error :messages="$errors->get('logo')" class="mt-2" />
-                </div>
-            </div>
-
-            {{-- Banner --}}
-            <div class="form-group row" id="form-panel-banner">
-                <div class="col-12 mb-3">
-                    <x-input-label for="banner" :value="__('models/ad.banner')"></x-input-label>
-                    <x-input-info :content="__('models/ad.banner_helper_info')" />
-                    <input type="file" name="banner" id="banner">
-                    <small id="banner-message" class="text-xs font-weight-bold"></small>
-                    <x-input-error :messages="$errors->get('banner')" class="mt-2" />
-                </div>
-            </div>
-
-            {{-- Caption --}}
-            <div class="form-group row" id="form-panel-caption">
-                <div class="col-12 mb-3">
-                    <x-input-label for="caption" :value="__('models/ad.caption')" />
-                    <x-input-info :content="__('models/ad.caption_helper_info')" />
-                    <textarea id="caption" name="caption" class="form-control" rows="3">{{ old('caption', $ad->caption) }}</textarea>
-                    <x-input-error :messages="$errors->get('caption')" class="mt-2" />
-                </div>
-            </div>
-
-            {{-- months_valid --}}
-            <div class="form-group row">
-                <div class="col-md-6 mb-3">
+                {{-- Months valid --}}
+                <div class="col-lg-6 col-md-12">
                     <x-input-label class="d-block" :value="__('models/ad.months_valid')" />
                     <x-input-info class="mb-3" :content="__('models/ad.months_valid_info')"></x-input-info>
                     <div class="custom-control custom-radio custom-control-inline">
@@ -144,7 +112,124 @@
                     </div>
                     <x-input-error :messages="$errors->get('months_valid')" class="mt-2" />
                 </div>
+            </div>
 
+            {{-- Banner --}}
+            <div class="form-group row" id="form-panel-banner">
+                <div class="col-12">
+                    <x-input-label for="banner" :value="__('models/ad.banner')" :required_tag="true"></x-input-label>
+                    <x-input-info :content="__('models/ad.banner_helper_info')" />
+                    <input type="file" name="banner" id="banner">
+                    <small id="banner-message" class="text-xs font-weight-bold"></small>
+                    <x-input-error :messages="$errors->get('banner')" class="mt-2" />
+                </div>
+            </div>
+
+            {{-- Caption --}}
+            <div class="form-group row" id="form-panel-caption">
+                <div class="col-12">
+                    <x-input-label for="caption" :value="__('models/ad.caption')" />
+                    <x-input-info :content="__('models/ad.caption_helper_info')" :required_tag="true"/>
+                    <textarea id="caption" name="caption" class="form-control" rows="5">{{ old('caption', $ad->caption) }}</textarea>
+                    <x-input-error :messages="$errors->get('caption')" class="mt-2" />
+                </div>
+            </div>
+
+            <h4 class="mt-5">{{ __('models/ad.company_data') }}</h4>
+            <hr>
+
+            {{-- Logo --}}
+            <div class="form-group row" id="form-panel-logo">
+                <div class="col-lg-4 col-md-6 col-sm-12">
+                    <x-input-label for="logo" :value="__('models/company.logo')"></x-input-label>
+                    <x-input-info :content="__('models/company.logo_helper_info')" :required_tag="true"/>
+                    <input type="file" name="logo" id="logo">
+                    <small id="logo-message" class="text-xs font-weight-bold"></small>
+                    <x-input-error :messages="$errors->get('logo')" class="mt-2" />
+                </div>
+            </div>
+
+            {{-- Company type select --}}
+            <div class="form-group row">
+                <div class="col-lg-4 col-md-12">
+                    <x-input-label for="company_type" :value="__('models/ad.company_type')" :required_tag="true"/>
+                    <select class="form-control border border-dark" id="company_type" name="company_type">
+                        @foreach($companyTypes as $id => $title)
+                            <option value="{{ $id }}" @selected((int)old('company_type', $ad->company_type->value) === $id)>{{ $title }}</option>
+                        @endforeach
+                    </select>
+                    <x-input-error :messages="$errors->get('company_type')" class="mt-2"/>
+                </div>
+            </div>
+
+
+            <div class="form-group row">
+                {{-- Company title --}}
+                <div class="col-lg-4 col-md-12">
+                    <x-input-label for="company_title" :value="__('models/ad.company_title')" :required_tag="true"></x-input-label>
+                    <x-text-input id="company_title"
+                                  name="company_title"
+                                  :value="old('company_title', $ad->company_title)"
+                                  placeholder="{{ __('models/ad.company_title_placeholder') }}"
+                    />
+                    <x-input-error :messages="$errors->get('company_title')" class="mt-2" />
+                </div>
+                {{-- Company address--}}
+                <div class="col-lg-8 col-md-12">
+                    <x-input-label for="company_address" :value="__('models/ad.company_address')" :required_tag="true"></x-input-label>
+                    <x-text-input id="company_address"
+                                  name="company_address"
+                                  :value="old('company_address', $ad->company_address)"
+                                  placeholder="{{ __('models/ad.company_address_placeholder') }}"
+                    />
+                    <x-input-error :messages="$errors->get('company_address')" class="mt-2" />
+                </div>
+            </div>
+
+            <div class="form-group row">
+                {{-- City id --}}
+                <div class="col-lg-4 col-md-12">
+                    <x-input-label for="city_id" :value="__('models/ad.city_id')" :required_tag="true"/>
+                    <select class="form-control border border-dark" id="city_id" name="city_id">
+                        @foreach($cities as $city)
+                            <option value="{{ $city->id }}" @selected((int)old('city_id', $ad->city_id) === $city->id)>{{ $city->title }}</option>
+                        @endforeach
+                    </select>
+                    <x-input-error :messages="$errors->get('city_id')" class="mt-2"/>
+                </div>
+                {{-- Company website --}}
+                <div class="col-lg-8 col-md-12">
+                    <x-input-label for="company_website" :value="__('models/ad.company_website')"></x-input-label>
+                    <x-text-input id="company_website"
+                                  name="company_website"
+                                  :value="old('company_website', $ad->company_website)"
+                                  placeholder="{{ __('models/ad.company_website_placeholder') }}"
+                    />
+                    <x-input-error :messages="$errors->get('company_website')" class="mt-2" />
+                </div>
+            </div>
+
+            <div class="form-group row">
+                {{-- Company phone --}}
+                <div class="col-lg-6 col-md-12">
+                    <x-input-label for="company_phone" :value="__('models/ad.company_phone')"></x-input-label>
+                    <x-text-input id="company_phone"
+                                  name="company_phone"
+                                  :value="old('company_phone', $ad->company_phone)"
+                                  placeholder="{{ __('models/ad.company_phone_placeholder') }}"
+                    />
+                    <x-input-error :messages="$errors->get('company_phone')" class="mt-2" />
+                </div>
+                {{-- Company mobile phone --}}
+                <div class="col-lg-6 col-md-12">
+                    <x-input-label for="company_mobile_phone" :value="__('models/ad.company_mobile_phone')"></x-input-label>
+                    <x-text-input id="company_mobile_phone"
+                                  name="company_mobile_phone"
+                                  :value="old('company_mobile_phone', $ad->company_mobile_phone)"
+                                  placeholder="{{ __('models/ad.company_mobile_phone_placeholder') }}"
+                    />
+                    <x-input-error :messages="$errors->get('company_mobile_phone')" class="mt-2" />
+                </div>
             </div>
 
             <div class="form-group row mt-5">
