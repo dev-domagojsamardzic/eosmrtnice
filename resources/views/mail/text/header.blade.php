a9c8ca0efee9f071821034006164ec69f672d418
dev-domagojsamardzic <dev.domagojsamardzic@gmail.com>
2024-12-05 22:11:46 +0100

    Add secure assets

--- resources/views/mail/text/header.blade.php
+++ resources/views/mail/text/header.blade.php
@@ -1,7 +1,7 @@
 @props(['url'])
 <tr>
     <td class="header">
-        <img style="height: 30px; width: auto;" src="{{ asset('graphics/logo/logo-light.svg') }}">
+        <img style="height: 30px; width: auto;" src="{{ secure_asset('graphics/logo/logo-light.svg') }}">
         <span>{{ config('app.name') }}</span>
     </td>
 </tr>
